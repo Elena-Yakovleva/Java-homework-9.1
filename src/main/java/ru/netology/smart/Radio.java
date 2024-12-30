c461349d9619c327be74fd01c2ba76a38d89bc1c
Elena <qa2301-mail.ru>
2024-12-30 21:37:30 +0300

    Lombok/@Getter

--- src/main/java/ru/netology/smart/Radio.java
+++ src/main/java/ru/netology/smart/Radio.java
@@ -2,9 +2,10 @@ package ru.netology.smart;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+
 @AllArgsConstructor
 @Data
 
@@ -12,13 +13,88 @@ public class Radio {
 
 
     private int minChannelNumber;
+    @Getter // getMaxChannelNumber();
     private int maxChannelNumber;
+    @Getter // getChannelNumber();
     private int channelNumber;
 
     private int minCurrentVolume;
     private int maxCurrentVolume;
+    @Getter // getCurrentVolume();
     private int currentVolume;
 
+    public Radio() {
+        this.minChannelNumber = 0;
+        this.maxChannelNumber = 9;
+
+        this.minCurrentVolume = 0;
+        this.maxCurrentVolume = 100;
+    }
+
+    public Radio(int size) {
+        this.maxChannelNumber = (size - 1);
+    }
+
+    public void setChannelNumber(int newChannelNumber) {
+        if (newChannelNumber < minChannelNumber) {
+            return;
+        }
+        if (newChannelNumber > maxChannelNumber) {
+            return;
+        } else {
+            this.channelNumber = newChannelNumber;
+        }
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < minCurrentVolume) {
+            return;
+        }
+        if (currentVolume > maxCurrentVolume) {
+            return;
+        } else {
+            this.currentVolume = currentVolume;
+        }
+    }
+
+    public void next() {
+        if (channelNumber < maxChannelNumber) {
+            channelNumber++;
+        } else {
+            channelNumber = minChannelNumber;
+        }
+    }
+
+    public void prev() {
+        if (channelNumber > minChannelNumber) {
+            channelNumber--;
+        } else {
+            channelNumber = maxChannelNumber;
+        }
+    }
+
+    public void setRadioNumber(int newChannelNumber) {
+        if (newChannelNumber < minChannelNumber) {
+            return;
+        }
+        if (newChannelNumber > maxChannelNumber) {
+            return;
+        } else {
+            this.channelNumber = newChannelNumber;
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxCurrentVolume) {
+            currentVolume++;
+        }
+    }
+
+    public void reduceVolume() {
+        if (currentVolume > minCurrentVolume) {
+            currentVolume--;
+        }
+    }
 
 }
 
